594d8cba68f646d56b60785a41769138a96912ba
sohaibahsan007 <sohaibahsan007@live.com>
2016-09-03 13:17:14 +0500

    ss

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
-var letterColors = [red, orange, green, blue, purple];
+var letterColors = [orange,red, green, blue, purple];
 
 drawName(myName, letterColors);
 
